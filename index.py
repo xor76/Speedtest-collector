e5a0605b96b4a91228f3e74647ec1d0bb197f4e0
Marco Salvi <marco.salvi@gmail.com>
2022-01-19 18:23:22 +0100

    slim version

--- index.py
+++ index.py
@@ -1,30 +1,12 @@
 import os
 import boto3
-import dateutil.parser as dp
 import traceback
 
 def storeSpeedTestResultDB(speedTestResultJson, dydbClient, dbTable):
     print("1nside store func"+'-'*20)
     print(speedTestResultJson)
-    print(speedTestResultJson['timestamp'])
-    print("2nside store func"+'-'*20)
-    timestamp_p = dp.parse(speedTestResultJson['timestamp'])
     response = dydbClient.put_item(TableName=dbTable,
-                               Item={
-                                   'ResultId': {"S": str(speedTestResultJson['result']['id'])},
-                                   'DownloadSpeed': {"N": str(speedTestResultJson['download']['bytes'])},
-                                   'TimeStamp': {"N": str(timestamp_p.timestamp())},
-                                   'UploadSpeed': {"N": str(speedTestResultJson['upload']['bytes'])},
-                                   'Latency': {"N": str(speedTestResultJson['ping']['latency'])},
-                                   'Jitter': {"N": str(speedTestResultJson['ping']['jitter'])},
-                                   'internalIp': {"S": str(speedTestResultJson['interface']['internalIp'])},
-                                   'externalIp': {"S": str(speedTestResultJson['interface']['externalIp'])},
-                                   'macAddr': {"S": str(speedTestResultJson['interface']['macAddr'])},
-                                   'packetLoss': {"N": str(speedTestResultJson['packetLoss'])},
-                                   'ServerName': {"S": str(speedTestResultJson['server']['name'])},
-                                   'ServerHost': {"S": str(speedTestResultJson['server']['host'])},
-                                   'ServerID': {"N": str(speedTestResultJson['server']['id'])},
-                               }
+                               Item=speedTestResultJson
                             )
     return response
 
