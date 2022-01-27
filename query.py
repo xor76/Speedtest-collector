1693a96bf60071e32aa6f8f32960949d9ae357fa
Marco Salvi <marco.salvi@gmail.com>
2022-01-27 16:10:46 +0100

    Fix json result

--- query.py
+++ query.py
@@ -15,10 +15,10 @@ def handler(event, context):
         TABLE_NAME = os.getenv('TABLE_NAME')
         dynamo = boto3.resource('dynamodb')
         res = query_SpeedTestResults(dynamo, TABLE_NAME)
-        body_str = '{'
+        body_str = '['
         for i in res[-24:]:
             body_str += '{"t":' +str(i['TimeStamp'])+ ', "dw": ' + str(i['DownloadSpeed']) + ', "up": ' + str(i['UploadSpeed']) + '},'
-        body_str = body_str[:-1] + '}'
+        body_str = body_str[:-1] + ']'
     except Exception as e:
         return {'statusCode': 502,
                 'body': 'Exception: ' + str(e) + '\nTraceback: ' + traceback.format_exc(),
